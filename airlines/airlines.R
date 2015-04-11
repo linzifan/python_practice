f46cd068ac140ee3234f66706cec3945a100a4c4
linzifan <zl2364@columbia.edu>
2015-04-11 00:05:36 -0400

    airlines

--- airlines/airlines.R
+++ airlines/airlines.R
@@ -1,3 +1,4 @@
+setwd("~/Documents/python/python_practice/airlines")
 CA <- read.csv("CA.csv", header = T)
 CA$CA <- "yes"
 MU <- read.csv("MU.csv", header = T)
@@ -11,3 +12,11 @@ allairline <- merge(allairline, CZ[, c(3,6)], by = "IATA", all = TRUE)
 allairline <- merge(allairline, HU[, c(3,6)], by = "IATA", all = TRUE)
 
 allairline <- read.csv("all_destinations_modified.csv", header = T)
+summary(allairline$Country[allairline$CA != "NULL"])
+bplt <- barplot(sort(summary(allairline$Country), decreasing = TRUE),  
+                beside=TRUE, horiz=TRUE)
+names(sort(summary(allairline$Country), decreasing = TRUE))
+# variable 'bplt' is now a matrix of vertical bar positions on the y-axis
+
+text(x= sort(summary(allairline$Country), decreasing = TRUE)+0.3, y= bplt, 
+     labels=sort(summary(allairline$Country), decreasing = TRUE), xpd=TRUE)
